46eece9fcc031fd33ba05db19dcbae7230408c0d
gram <gram@DESKTOP-E54V6V8>
2025-03-06 11:11:53 +0900

    BadException이 RuntimeException을 상속받았기 때문에 throws 선언 없이도 예외를 던질 수 있음

--- src/calculator/ModOperator.java
+++ src/calculator/ModOperator.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class ModOperator<T extends Number> implements Operator<T> {
     @Override
-    public double operate(T first, T second) throws BadException {
+    public double operate(T first, T second) {
         if (second.doubleValue() == 0) {
             throw new BadException("0으로 나눌 수 없음");
         } else {
